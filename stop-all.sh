677c5e17ef094f4e973726357481c5c8b1789e76
Michael Schwartz <mykesx@gmail.com>
2019-01-28 16:42:33 +0000

    add stop-all script

---
+++ stop-all.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+
+docker stop weather-microservice
+docker stop tvguide-microservice
+docker stop autelis-microservice
+docker stop tivo-microservice
+docker stop denon-microservice
+docker stop harmony-microservice
+docker stop mqtt-bridge
+docker stop bravia-microservice
+docker stop lgtv-microservice
+docker stop nest-microservice
+docker stop macro-microservice
+
+docker stop mosca
+docker stop mongodb
+
