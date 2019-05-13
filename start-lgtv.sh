27a23268949804b4262618f061230072a18828e0
Michael Schwartz <mykesx@gmail.com>
2019-05-13 09:53:44 -0700

    fix lgtv start

--- start-lgtv.sh
+++ start-lgtv.sh
@@ -13,9 +13,10 @@ docker pull robodomo/$SERVICE
 echo "starting new $SERVICE"
 docker run \
     -d \
+    --net=host \
     --restart always \
     --name $SERVICE \
-    -e LGTV_HOSTS=olede6p \
+    -e LGTV_HOSTS="olede6p;78:5d:c8:e6:3f:ef" \
     -e MQTT_HOST=mqtt://ha \
     robodomo/$SERVICE
 
