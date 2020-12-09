3130808448505d24e43f5c95849a6b2b11b7cee7
Michael Schwartz <mykesx@gmail.com>
2020-12-09 12:43:45 -0800

    new service script

---
+++ start-sysinfo.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#### ENV VARS
+
+# You can set these in this script (uncomment and edit the lines) or set them in your .zshrc/.bashrc/etc.
+
+# Change this to match your MQTT broker hostname:
+if [ "$MQTT_HOST" = "" ]; then
+  MQTT_HOST="mqtt://mqtt"
+fi
+
+# Change this to match your MONGODB hostname:
+if [ "$MONGO_URL" = "" ]; then
+  MONGO_URL="mongodb://mongodb"
+fi
+
+  
+#### /ENV VARS
+
+SERVICE=sysinfo-microservice
+
+DEBUG="sysinfo"
+#DEBUG="HostBase,sysinfo" \
+
+
+echo "stopping $SERVICE"
+docker stop $SERVICE
+
+echo "removing old $SERVICE"
+docker rm $SERVICE
+
+echo "pulling $SERVICE"
+docker pull robodomo/$SERVICE
+
+echo "starting new $SERVICE"
+docker run \
+    -d \
+    --log-opt max-size=10m --log-opt max-file=5 \
+    --net=host \
+    --restart always \
+    --name $SERVICE \
+    --expose 4000 \
+    -e DEBUG="$DEBUG" \
+    -e MQTT_HOST=$MQTT_HOST \
+    -e TITLE=$SERVICE \
+    robodomo/$SERVICE
+
