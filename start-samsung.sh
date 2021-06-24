b9848bc75b64a0ece3f949caf49011ea2e146f35
Michael Schwartz <mykesx@gmail.com>
2021-06-24 12:43:54 -0700

    add start script for samsung-microservice

---
+++ start-samsung.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#### ENV VARS
+
+# You can set these in this script (uncomment and edit the lines) or set them in your .zshrc/.bashrc/etc.
+
+# Set this to a comma separated list of AVR hostnames/IP addresses that are to be controlled by
+# this microservice
+# samsung_HOSTS=samsung1,samsung2,...
+
+# Change this to match your MQTT broker hostname:
+if [ "$MQTT_HOST" = "" ]; then
+  MQTT_HOST="mqtt://mqtt"
+fi
+
+# Change this to match your MONGODB hostname:
+if [ "$MONGO_URL" = "" ]; then
+  MONGO_URL="mongodb://mongodb"
+fi
+
+#### /ENV VARS
+
+SERVICE=samsung-microservice
+
+echo "stopping $SERVICE"
+docker stop $SERVICE
+
+echo "removing old $SERVICE"
+docker rm $SERVICE
+
+echo "pulling $SERVICE"
+docker pull robodomo/$SERVICE
+
+echo "starting new $SERVICE"
+docker run \
+    -d \
+    --restart always \
+    --name $SERVICE \
+    -e MQTT_HOST=$MQTT_HOST \
+    -e ROBODOMO_MONGODB=$MONGO_URL \
+    -e MONGODB=$MONGO_URL \
+    -e TITLE=$SERVICE \
+    robodomo/$SERVICE
+
