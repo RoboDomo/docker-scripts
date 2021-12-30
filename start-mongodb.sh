a8eb4b4f3c67eb2739e45f919948ce78b8f53ff0
Michael Schwartz <mykesx@gmail.com>
2021-12-29 17:24:49 -0800

    update mongodb version

--- start-mongodb.sh
+++ start-mongodb.sh
@@ -26,5 +26,5 @@ docker rm $SERVICE
 echo "pulling $SERVICE"
 docker pull mongo:3.4
 
-docker run $docker_parameters -p 27017:27017 -v ~/data:/data/db mongo:3.4
+docker run $docker_parameters -p 27017:27017 -v ~/data:/data/db mongo:3.6
 
